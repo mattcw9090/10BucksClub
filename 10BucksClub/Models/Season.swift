ffb6ea5ee94d1fc27f2acc07ac5d6a026642c010
mattcw9090 <mattcw9090@gmail.com>
2024-12-28 10:49:16 +0800

    Tweaked Season and Session model files to reflect cardinality accurately

--- 10BucksClub/Models/Season.swift
+++ 10BucksClub/Models/Season.swift
@@ -5,7 +5,8 @@ import SwiftData
 class Season {
     @Attribute(.unique) var seasonNumber: Int
     var isCompleted: Bool
-    
+    @Relationship(inverse: \Session.season) var sessions: [Session] = []
+
     init(seasonNumber: Int, isCompleted: Bool = false) {
         self.seasonNumber = seasonNumber
         self.isCompleted = isCompleted
--- 10BucksClub/Models/Session.swift
+++ 10BucksClub/Models/Session.swift
@@ -4,12 +4,12 @@ import SwiftData
 @Model
 class Session {
     @Attribute(.unique) var id: UUID
-    var number: Int
+    var sessionNumber: Int
     @Relationship var season: Season
-    
+
     init(id: UUID = UUID(), number: Int, season: Season) {
         self.id = id
-        self.number = number
+        self.sessionNumber = number
         self.season = season
     }
 }
