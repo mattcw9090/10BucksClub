c37d950f122b8d7eb93c4d424e536a3f8bd033ed
mattcw9090 <mattcw9090@gmail.com>
2024-12-30 09:46:38 +0800

    test

--- 10BucksClub/Models/Player.swift
+++ 10BucksClub/Models/Player.swift
@@ -3,33 +3,26 @@ import SwiftData
 
 @Model
 class Player {
-    @Attribute(.unique)
-    var id: UUID
-
+    @Attribute(.unique) var id: UUID
     var name: String
     var statusRawValue: String
     var waitlistPosition: Int?
-
-    init(
-        id: UUID = UUID(),
-        name: String,
-        status: PlayerStatus = .notInSession,
-        waitlistPosition: Int? = nil
-    ) {
-        self.id = id
-        self.name = name
-        self.statusRawValue = status.rawValue
-        self.waitlistPosition = waitlistPosition
+    
+    enum PlayerStatus: String, Codable, CaseIterable {
+        case playing = "Currently Playing"
+        case onWaitlist = "On the Waitlist"
+        case notInSession = "Not in Session"
     }
-
+    
     var status: PlayerStatus {
         get { PlayerStatus(rawValue: statusRawValue) ?? .notInSession }
         set { statusRawValue = newValue.rawValue }
     }
 
-    enum PlayerStatus: String, Codable, CaseIterable {
-        case playing = "Currently Playing"
-        case onWaitlist = "On the Waitlist"
-        case notInSession = "Not in Session"
+    init(id: UUID = UUID(), name: String, status: PlayerStatus = .notInSession, waitlistPosition: Int? = nil) {
+        self.id = id
+        self.name = name
+        self.statusRawValue = status.rawValue
+        self.waitlistPosition = waitlistPosition
     }
 }
